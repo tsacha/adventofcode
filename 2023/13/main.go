2535271b1624a6423572b8d9ee33e2dddde792ed
Sacha Trémoureux <sacha@tremoureux.fr>
2023-12-17 19:35:59 +0100

    2023/12/13 part2

--- 2023/13/main.go
+++ 2023/13/main.go
@@ -25,15 +25,25 @@ type puzzle struct {
 	vertical   []line
 }
 
-func find_reflections(line []line, i int, j int, ret int, is_child bool) (reflections int, err error) {
+func find_reflections(line []line, i int, j int, ret int, reverse bool, mult int, skip_result int, is_child bool) (reflections int, err error) {
 	reflections = ret
 	for x := j; x > i; x-- {
 		if reflect.DeepEqual(line[i], line[x]) {
 			if x-i == 1 {
-				return x, nil
-			} else {
-				reflections, _ = find_reflections(line, i+1, x-1, reflections, true)
+				var result int
+				if !reverse {
+					result = mult * x
+				} else {
+					result = mult * (len(line) - x)
+				}
+
+				if skip_result != result {
+					return result, nil
+				} else {
+					skip_result = -1
+				}
 			}
+			reflections, _ = find_reflections(line, i+1, x-1, reflections, reverse, mult, skip_result, true)
 		} else if is_child {
 			return reflections, nil
 		}
@@ -46,40 +56,80 @@ func find_reflections(line []line, i int, j int, ret int, is_child bool) (reflec
 	}
 }
 
-func (p *puzzle) solve_puzzle() (s int) {
+func (p *puzzle) solve_puzzle(skip_result int) (s int) {
 	var err error
-	r := 0
 
-	r, err = find_reflections(p.vertical, 0, len(p.vertical)-1, -1, false)
+	s, err = find_reflections(p.vertical, 0, len(p.vertical)-1, -1, false, 1, skip_result, false)
 	if err == nil {
-		s += r
+		return s
 	}
 	slices.Reverse(p.vertical)
-	r, err = find_reflections(p.vertical, 0, len(p.vertical)-1, -1, false)
+	s, err = find_reflections(p.vertical, 0, len(p.vertical)-1, -1, true, 1, skip_result, false)
+	slices.Reverse(p.vertical)
 	if err == nil {
-		s += len(p.vertical) - r
+		return s
 	}
 
-	r, err = find_reflections(p.horizontal, 0, len(p.horizontal)-1, -1, false)
+	s, err = find_reflections(p.horizontal, 0, len(p.horizontal)-1, -1, false, 100, skip_result, false)
 	if err == nil {
-		s += r * 100
+		return s
 	}
 	slices.Reverse(p.horizontal)
-	r, err = find_reflections(p.horizontal, 0, len(p.horizontal)-1, -1, false)
+	s, err = find_reflections(p.horizontal, 0, len(p.horizontal)-1, -1, true, 100, skip_result, false)
+	slices.Reverse(p.horizontal)
 	if err == nil {
-		s += (len(p.horizontal) - r) * 100
+		return s
 	}
 
-	return s
+	return 0
 }
 
-func part1() (solution int) {
+func (p *puzzle) solve_smudge_puzzle() (s int) {
+	initial_solve := p.solve_puzzle(-1)
+	for ph := 0; ph < len(p.horizontal); ph++ {
+		for pv := 0; pv < len(p.horizontal[0]); pv++ {
+			s := puzzle{}
+			s.horizontal = []line{}
+			s.vertical = []line{}
+
+			for h := 0; h < len(p.horizontal); h++ {
+				s.horizontal = append(s.horizontal, line{})
+				for v := 0; v < len(p.horizontal[0]); v++ {
+					s.horizontal[h] = append(s.horizontal[h], p.horizontal[h][v])
+				}
+			}
+
+			if s.horizontal[ph][pv] == ASH {
+				s.horizontal[ph][pv] = ROCK
+			} else if s.horizontal[ph][pv] == ROCK {
+				s.horizontal[ph][pv] = ASH
+			}
+
+			for v := 0; v < len(s.horizontal[0]); v++ {
+				s.vertical = append(s.vertical, line{})
+				for h := 0; h < len(s.horizontal); h++ {
+					s.vertical[v] = append(s.vertical[v], s.horizontal[h][v])
+				}
+			}
+
+			solve := s.solve_puzzle(initial_solve)
+			if solve > 0 {
+				fmt.Printf("[%d:%d] %d (was %d)\n", ph, pv, solve, initial_solve)
+				return solve
+			}
+		}
+	}
+
+	return initial_solve
+}
+
+func part(part int) (solution int) {
 	solution = 0
+
 	input := string(utils.PuzzleInput(2023, 13))
 
-	// example
-	_ = `
-#.##..##.
+	// examples
+	_ = `#.##..##.
 ..#.##.#.
 ##......#
 ##......#
@@ -95,7 +145,7 @@ func part1() (solution int) {
 ..##..###
 #....#..#`
 
-	for puzzle_nb, puzzle_str := range strings.Split(strings.Trim(input, "\n"), "\n\n") {
+	for _, puzzle_str := range strings.Split(strings.Trim(input, "\n"), "\n\n") {
 		p := puzzle{}
 		p.horizontal = []line{}
 		p.vertical = []line{}
@@ -126,13 +176,18 @@ func part1() (solution int) {
 			}
 		}
 
-		p_sol := p.solve_puzzle()
-		fmt.Printf("[%d] %d\n", puzzle_nb, p_sol)
-		solution += p_sol
+		if part == 1 {
+			p_sol := p.solve_puzzle(-1)
+			solution += p_sol
+		} else if part == 2 {
+			p_sol := p.solve_smudge_puzzle()
+			solution += p_sol
+		}
 	}
 	return solution
 }
 
 func main() {
-	fmt.Printf("Solution 1: %d\n", part1())
+	//fmt.Printf("Solution 1: %d\n", part(1))
+	fmt.Printf("Solution: %d\n", part(2))
 }
