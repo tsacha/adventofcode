08c2384cb928727dd357552f64b712379216909e
Sacha Trémoureux <sacha@tremoureux.fr>
2023-12-27 13:03:28 +0100

    2023/12/17 part0

--- 2023/17/main.go
+++ 2023/17/main.go
@@ -16,13 +16,15 @@ type vertex struct {
 	y int
 	v int
 
-	w int
-	p *vertex
+	dist int
+	prev *vertex
 
 	in bool
 }
+type graph []*vertex
 type grid struct {
-	vertices [][]vertex
+	vertices [][]*vertex
+	graph    graph
 
 	xSize int
 	ySize int
@@ -30,71 +32,70 @@ type grid struct {
 
 func importGrid(input string) grid {
 	g := grid{}
+	q := graph{}
 	for x, row := range strings.Split(strings.Trim(input, "\n"), "\n") {
-		g.vertices = append(g.vertices, []vertex{})
+		g.vertices = append(g.vertices, []*vertex{})
 		for y, c := range strings.Split(row, "") {
-			v, _ := strconv.Atoi(c)
-			g.vertices[x] = append(g.vertices[x], vertex{
-				x: x,
-				y: y,
-				w: math.MaxInt,
-				v: v,
-			})
+			value, _ := strconv.Atoi(c)
+			v := vertex{
+				x:    x,
+				y:    y,
+				v:    value,
+				dist: math.MaxInt,
+			}
+			g.vertices[x] = append(g.vertices[x], &v)
+			q = append(q, &v)
 		}
 	}
 	g.xSize = len(g.vertices)
 	g.ySize = len(g.vertices[0])
+	g.graph = q
 
 	return g
 }
 
 func (g *grid) getNeighbors(v *vertex) (vertices []*vertex) {
 	if v.x > 0 {
-		vertices = append(vertices, &g.vertices[v.x-1][v.y]) // left
+		vertices = append(vertices, g.vertices[v.x-1][v.y]) // left
 	}
 	if v.y > 0 {
-		vertices = append(vertices, &g.vertices[v.x][v.y-1]) // up
+		vertices = append(vertices, g.vertices[v.x][v.y-1]) // up
 	}
 	if v.x+1 < g.xSize {
-		vertices = append(vertices, &g.vertices[v.x+1][v.y]) // right
+		vertices = append(vertices, g.vertices[v.x+1][v.y]) // right
 	}
 	if v.y+1 < g.ySize {
-		vertices = append(vertices, &g.vertices[v.x][v.y+1]) // down
+		vertices = append(vertices, g.vertices[v.x][v.y+1]) // down
 	}
 	return vertices
 }
 
-func (g *grid) minNeighbor(q []*vertex) (mn *vertex) {
+func (g *grid) minVertex(q []*vertex) (mn *vertex) {
 	min := math.MaxInt
 	for _, v := range q {
-		if v.w < min {
-			min = v.w
+		if v.dist < min {
+			min = v.dist
 			mn = v
 		}
 	}
 	return mn
 }
 
-func (g *grid) updateDistance(v1 *vertex, v2 *vertex) {
-	if v2.w > v1.w+v1.v {
-		v2.w = v1.w + v1.v
-		v2.p = v1
+func (g *grid) updateWeight(v1 *vertex, v2 *vertex) {
+	if v2.dist > v1.dist+v1.v {
+		v2.dist = v1.dist + v1.v
+		v2.prev = v1
 	}
 }
 
 func (g *grid) shortestPath() int {
-	q := []*vertex{}
-	start := &g.vertices[0][0]
-	start.w = 0
+	q := make(graph, len(g.graph))
+	copy(q, g.graph)
 
-	for x := range g.vertices {
-		for y := range g.vertices[x] {
-			q = append(q, &g.vertices[x][y])
-		}
-	}
-
+	start := g.vertices[0][0]
+	start.dist = 0
 	for len(q) != 0 {
-		v1 := g.minNeighbor(q)
+		v1 := g.minVertex(q)
 		for n, v := range q {
 			if v == v1 {
 				q = slices.Delete(q, n, n+1)
@@ -102,17 +103,17 @@ func (g *grid) shortestPath() int {
 			}
 		}
 		for _, v2 := range g.getNeighbors(v1) {
-			g.updateDistance(v1, v2)
+			g.updateWeight(v1, v2)
 		}
 	}
 
-	s := []*vertex{}
-	cursor := &g.vertices[g.xSize-1][g.ySize-1]
+	s := graph{}
+	cursor := g.vertices[g.xSize-1][g.ySize-1]
 
 	for start != cursor {
 		s = append(s, cursor)
 		cursor.in = true
-		cursor = cursor.p
+		cursor = cursor.prev
 	}
 
 	sum := 0
@@ -142,6 +143,11 @@ func (g *grid) print() {
 
 func part0() (result int) {
 	input := string(utils.PuzzleInput(2023, 17))
+	_ = `
+123
+456
+789
+`
 	grid := importGrid(input)
 	result = grid.shortestPath()
 	grid.print()
